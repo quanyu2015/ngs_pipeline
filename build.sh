f195f3fe5781096aed0ef16189db0c58dddd4d87
etheleon <picy2k@gmail.com>
2016-05-18 17:10:54 +0800

    update Dockerfile with pass; seems to be working

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ docker build -t sortmerna - < tools/sortmerna/Dockerfile
 docker build -t illumiprocessor - < tools/illumiprocessor/Dockerfile
 docker build -t diamond - < tools/diamond/Dockerfile
 docker build -t megan - < tools/megan/Dockerfile
+docker build -t pass - < tools/pAss/Dockerfile
 
