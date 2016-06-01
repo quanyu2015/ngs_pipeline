06e441d6fde93df3c11c77f534c3f1a806523530
etheleon <picy2k@gmail.com>
2016-06-01 17:09:48 +0800

    update build with newbler and newer install

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ docker build -t sortmerna - < tools/sortmerna/Dockerfile
 docker build -t illumiprocessor - < tools/illumiprocessor/Dockerfile
 docker build -t diamond - < tools/diamond/Dockerfile
 docker build -t megan - < tools/megan/Dockerfile
-docker build -t pass - < tools/pAss/Dockerfile
+docker build -t pass - < tools/pAss/Dockerfile2
+docker build -t newbler  - < tools/assemblyNewbler/Dockerfile
 
